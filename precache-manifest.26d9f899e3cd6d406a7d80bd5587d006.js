3b9416655fc2ff1a63bb0368e85fcfd382b26ca8
Jucélio Brandão <juceneon@gmail.com>
2021-09-02 23:00:56 -0300

    Updates

---
+++ precache-manifest.26d9f899e3cd6d406a7d80bd5587d006.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "948b631ddb0d29ec78e9bd2d9b9d2f0a",
+    "url": "/exercise-pokedex-state/index.html"
+  },
+  {
+    "revision": "69d8336b4e3580c83e00",
+    "url": "/exercise-pokedex-state/static/css/main.3fa6c9ee.chunk.css"
+  },
+  {
+    "revision": "4591a0f6603ea40a9395",
+    "url": "/exercise-pokedex-state/static/js/2.b41d3823.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/exercise-pokedex-state/static/js/2.b41d3823.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa6b68149272934a50c",
+    "url": "/exercise-pokedex-state/static/js/3.602028b6.chunk.js"
+  },
+  {
+    "revision": "69d8336b4e3580c83e00",
+    "url": "/exercise-pokedex-state/static/js/main.9aaeed72.chunk.js"
+  },
+  {
+    "revision": "dad4c69ef25369fba6c2",
+    "url": "/exercise-pokedex-state/static/js/runtime-main.74b27d45.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/exercise-pokedex-state/precache-manifest.26d9f899e3cd6d406a7d80bd5587d006.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/exercise-pokedex-state/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
